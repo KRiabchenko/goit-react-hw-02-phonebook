e41947a9febdf087f3129f38703d394d07b16265
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-12-07 13:38:06 +0200

    Create ContactForm.styled.js

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  width: auto;
+  padding: 15px;
+  border: 1px solid #454545;
+`;
+
+export const Label = styled.label`
+  display: flex;
+
+  justify-content: space-between;
+  padding: 15px;
+`;
+
+export const AddButton = styled.button`
+  width: 150px;
+  display: block;
+  margin: 0 auto;
+  margin-top: 5px;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
